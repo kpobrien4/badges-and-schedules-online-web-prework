07e140995b22a725b2d09d21503e266bfa3b946a
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-02 00:56:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+Name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(Name)
+  puts "Hello, my name is #{Name}."
+end
+
+def assign_rooms
+
+end
